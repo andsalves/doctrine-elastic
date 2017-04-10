231312f0335cc2624dabb6e745e3025c16522253
Anderson <ands.alves.nunes@gmail.com>
2017-04-09 22:36:05 -0300

    Fixing build 2

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/** @var \Composer\Autoload\ClassLoader $loader */
-if (file_exists(getcwd() . '../vendor/autoload.php')) {
-    $loader = include_once(getcwd() . '../vendor/autoload.php');
-}
