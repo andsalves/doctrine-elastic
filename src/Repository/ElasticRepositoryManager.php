85cf53bc5e34d737bbf03642e9db4ecd30932cf4
Anderson <ands.alves.nunes@gmail.com>
2017-05-29 15:50:25 -0300

    Allowing custom repositories per entity

--- src/Repository/ElasticRepositoryManager.php
+++ src/Repository/ElasticRepositoryManager.php
@@ -2,6 +2,8 @@
 
 namespace DoctrineElastic\Repository;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\Mapping\Entity;
 use DoctrineElastic\ElasticEntityManager;
 
 /**
@@ -12,14 +14,35 @@ class ElasticRepositoryManager {
     /** @var ElasticEntityRepository[] */
     protected $repositoryList;
 
+    /** @var AnnotationReader */
+    protected $annotationReader;
+
     public function getRepository(ElasticEntityManager $entityManager, $className) {
         $hashKey = md5($className) . spl_object_hash($entityManager);
 
         if (!isset($this->repositoryList[$hashKey])) {
-            $repository = new ElasticEntityRepository($entityManager, $className);
+            $repositoryClass = ElasticEntityRepository::class;
+
+            /** @var Entity $entityAnnotation */
+            $entityAnnotation = $this->getAnnotationReader()->getClassAnnotation(
+                new \ReflectionClass($className), Entity::class
+            );
+            if (!is_null($entityAnnotation) && isset($entityAnnotation->repositoryClass)) {
+                $repositoryClass = $entityAnnotation->repositoryClass;
+            }
+
+            $repository = new $repositoryClass($entityManager, $className);
             $this->repositoryList[$hashKey] = $repository;
         }
 
         return $this->repositoryList[$hashKey];
+    }
+
+    public function getAnnotationReader() {
+        if (is_null($this->annotationReader)) {
+            $this->annotationReader = new AnnotationReader();
+        }
+
+        return $this->annotationReader;
     }
 }
